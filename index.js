0fe31321d19e277f87e5be86c4a51b6578f1f846
uchuika <robotensa@gmail.com>
2026-01-11 20:34:05 +0900

    expressを使用してレスポンスが返ってくるようにした

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const VC_DELETE_COOLDOWN = 60 * 1000; // 60秒
 
 const client = new Client({ intents: Object.values(GatewayIntentBits).reduce((a, b) => a | b) });
 
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello, world!',
+  })
+})
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`)
+})
+
 client.commands = new Collection();
 
 client.on('ready', () => {
